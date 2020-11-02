89ff120390370d5bed15998912b06d0fdb50a35d
Nicolas ROGE <nicolas.roge.contact@gmail.com>
2020-11-02 16:14:20 +0100

    Start Entities script - Nicolas

---
+++ Assets/Scripts/EntitiesStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EntitiesStats", menuName = "EntitiesStats/New Entity", order = 50)]
+public class EntitiesStats : MonoBehaviour
+{
+    public enum Type
+    {
+        Tour,
+        Robot,
+    }
+
+    public Type _class;
+
+    [Header("Stats")]
+    public int _HealthBase;
+    public int _SpeedBase;
+}
