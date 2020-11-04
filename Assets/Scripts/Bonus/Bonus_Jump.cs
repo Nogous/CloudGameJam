f3450f18e3228631cf55cf873782877eaa692db3
Camille <camille.lavarde@gmail.com>
2020-11-04 19:53:14 +0100

    jump

--- Assets/Scripts/Bonus/Bonus_Jump.cs
+++ Assets/Scripts/Bonus/Bonus_Jump.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Bonus_Jump : Bonus
 {
+    protected override void Start()
+    {
+        base.Start();
+        ActiveBonus();
+    }
+
     protected override void V_ActiveBonus()
     {
         base.V_ActiveBonus();
         myEntity.Jump();
+    }
+
+    protected override void EndBonus()
+    {
+        base.EndBonus();
+        myEntity.EndJump();
     }
 }
