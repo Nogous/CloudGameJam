d1df66a6760a8076ce83c20eb0ed7d3dbb28e70c
Camille <camille.lavarde@gmail.com>
2020-11-05 16:43:53 +0100

    nexus, stomper, tower

---
+++ Assets/Scripts/Entities Scripts/Tower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private Entity entity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (entity._EnnemyEntities.Count>0)
+        {
+            transform.forward = entity._EnnemyEntities[0].transform.position - transform.position;
+        }
+    }
+}
